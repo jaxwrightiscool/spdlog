68a3bb68999974fcec47a9e1816dcd4262772eeb
Jackson Wright <109768823+jaxwrightiscool@users.noreply.github.com>
2022-07-22 15:46:05 -0700

    Update test_daily_logger.cpp

--- tests/test_daily_logger.cpp
+++ tests/test_daily_logger.cpp
@@ -30,35 +30,23 @@ TEST_CASE("daily_logger with dateonly calculator", "[daily_logger]")
     prepare_logdir();
 
     // calculate filename (time based)
-    spdlog::filename_t basename = SPDLOG_FILENAME_T("test_logs/daily_dateonly");
-    std::tm tm = spdlog::details::os::localtime();
-    filename_memory_buf_t w;
-    spdlog::fmt_lib::format_to(
-        std::back_inserter(w), SPDLOG_FILENAME_T("{}_{:04d}-{:02d}-{:02d}"), basename, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
-
-    auto logger = spdlog::create<sink_type>("logger", basename, 0, 0);
-    for (int i = 0; i < 10; ++i)
-    {
-
-        logger->info("Test message {}", i);
-    }
-    logger->flush();
-
-    require_message_count(filename_buf_to_utf8string(w), 10);
-}
-
-struct custom_daily_file_name_calculator
-{
-    static spdlog::filename_t calc_filename(const spdlog::filename_t &basename, const tm &now_tm)
-    {
-        filename_memory_buf_t w;
-        spdlog::fmt_lib::format_to(std::back_inserter(w), SPDLOG_FILENAME_T("{}{:04d}{:02d}{:02d}"), basename, now_tm.tm_year + 1900,
-            now_tm.tm_mon + 1, now_tm.tm_mday);
-
-        return SPDLOG_BUF_TO_STRING(w);
-    }
-};
-
+    #ifdef SPDLOG_WCHAR_FILENAMES
+    spdlog::memory_buf_t buf;
+#    ifdef SPDLOG_USE_STD_FORMAT
+    spdlog::details::os::wstr_to_utf8buf(w, buf);
+    auto &filename = buf;
+#    else
+    spdlog::details::os::wstr_to_utf8buf(fmt::to_string(w), buf);
+    auto filename = fmt::to_string(buf);
+#    endif
+#else
+#    ifdef SPDLOG_USE_STD_FORMAT
+    auto &filename = w;
+#    else
+    auto filename = fmt::to_string(w);
+#    endif
+#endif
+    require_message_count(filename, 10);
 TEST_CASE("daily_logger with custom calculator", "[daily_logger]")
 {
     using sink_type = spdlog::sinks::daily_file_sink<std::mutex, custom_daily_file_name_calculator>;
